eb5bdbb31a943a17877986bb1172ed4396d3e34e
OwenVm <owen.vm3648@gmail.com>
2023-07-19 17:26:46 +0200

    first commit

---
+++ run.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from rv import radial_velocity, rv_error, remove_nans
+
+id = input("Enter the star's program number: ")
+
+hdul = fits.open(f'{id}_HRF_OBJ_ext_CosmicsRemoved_log_merged_cf.fits')
+
+flux = hdul[0].data.T
+L0 = hdul[0].header['CRVAL1']
+step = hdul[0].header['CDELT1']
+
+logL = np.linspace(L0, L0+len(flux)*step, len(flux))
+lam = np.exp(logL)
+
+flux_removed, wavelength_removed = remove_nans(flux)
+wavelength_good = lam[wavelength_removed]
+data = np.array([wavelength_good, flux_removed])
+
+name = input("Enter the star's name (this will be the name of the asc file): ")
+
+np.savetxt(f'{name}.asc', data.T, delimiter='\t')
+
+v0 = 2*float(input("Enter the star's radial velocity in km/s from SIMBAD: "))
+
+cutoff = 100
+new_cuttof = input("Enter the cutoff value for the peak finding algorithm (standard 100): ")
+if new_cuttof != '':
+    cutoff = float(new_cuttof)
+
+#lines = [6546.238, 6569.214]
+new_lines = input("If you want to use different lines, give name of the txt file: ") + ".txt"
+if new_lines != '':
+    lines = np.loadtxt(new_lines)
+
+a, b = radial_velocity(lines, flux, lam, v0, cutoff)
+c = rv_error(lines, flux, lam, v0, cutoff)
+
+print(f'Radial velocities of the star in km/s: {b:.5f} pm {c:.5f}')
+
---
+++ rv.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+def remove_nans(flux):
+    '''
+    removes all the nans from flux and returns the flux and the indexes of the non nans
+    '''
+    indexes = []
+    for i in range(len(flux)):
+        if ~np.isnan(flux[i]):
+            indexes.append(i)
+    return flux[indexes], indexes
+
+def radial_velocity_shift(lam, lam0):
+    ''' 
+    lam: observed wavelength of peak
+    lam0: rest wavelength of peak
+    returns velocity of the star in km/s
+    '''
+    c = 299792.458 # km/s
+    return c*(lam-lam0)/lam0
+
+def local_continuum(flux, lam, lam_lower, lam_upper):
+    ''' 
+    lam_lower: lower bound of the wavelength range of the peak of interest, assuming initial guess for v0
+    lam_upper: upper bound of the wavelength range of the peak of interest, assuming initial guess for v0
+    returns the median of the flux in the wavelength range
+    '''
+    return np.median(flux[(lam>lam_lower) & (lam<lam_upper)])
+
+def find_peak(flux, lam, lam0, v0, cutoff):
+    '''
+    returns the observed wavelength of the peak
+    '''
+    lam_lower = lam0*(1-v0/299792)
+    lam_upper = lam0*(1+v0/299792)
+    lc = local_continuum(flux, lam, lam_lower, lam_upper)
+    wavelength_range = lam[np.where((lam>lam_lower) & (lam<lam_upper))]
+    
+    peak = []
+    indexes = []
+    for i in wavelength_range:
+        flux_value = flux[np.where(lam==i)]
+        if np.abs(flux_value - lc) > cutoff:
+            peak.append(i)
+            indexes.append(np.where(lam==i)[0][0])
+    
+    if len(peak) == 0:
+        print('No peak found, maybe increase v0 or decrease the cutoff.')
+        return
+    
+    return lam[np.where(flux==min(flux[indexes]))[0]][0]
+        
+def find_rv_peak(flux, lam, lam0, v0, cutoff):
+    '''
+    returns the radial velocity of the star in km/s
+    '''
+    observed = find_peak(flux, lam, lam0, v0, cutoff)
+    return radial_velocity_shift(observed, lam0)
+
+def radial_velocity(lines, flux, lam, v0, cutoff):
+    '''
+    lines: list of rest wavelengths of the peaks of interest in angstrom
+    flux: flux of the star, arbitrary units
+    lam: wavelength values of the spectrum in angstrom
+    v0: initial guess of the radial velocity of the star in km/s (for example from SIMBAD)
+    cutoff: cutoff value for the peak, the higher the value, the more sensitive the peak finding algorithm
+    returns the radial velocities of the star in km/s for each peak and the mean radial velocity
+    '''
+    rv = []
+    for line in lines:
+        rv.append(find_rv_peak(flux, lam, line, v0, cutoff))
+    return rv, np.mean(rv)
+
+def rv_error(lines, flux, lam, v0, cutoff):
+    '''
+    lines: list of rest wavelengths of the peaks of interest in angstrom
+    flux: flux of the star, arbitrary units
+    lam: wavelength values of the spectrum in angstrom
+    v0: initial guess of the radial velocity of the star in km/s (for example from SIMBAD)
+    cutoff: cutoff value for the peak, the higher the value, the more sensitive the peak finding algorithm
+    uses error propagation to calculate the error of the radial velocity, given a list of radial velocities from different lines
+    '''
+    rv, mean = radial_velocity(lines, flux, lam, v0, cutoff)
+    return np.sqrt(np.sum((rv-mean)**2)/(len(rv)-1))
